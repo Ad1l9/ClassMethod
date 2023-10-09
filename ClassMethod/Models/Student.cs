a0262fa58d39dc8df91f05aeb83261d290794d21
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-09 06:36:42 +0400

    Add project files.

---
+++ ClassMethod/Models/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod.Models
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public ushort Point { get; set; }
+        public bool IsGraduated { get; set; }
+
+        public Student(string name, string surname, string group, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+            if (point <= 65)
+                IsGraduated = false;
+            else IsGraduated = true;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(@$"
+Name: {Name},
+Surname: {Surname},
+Group: {Group}
+Point: {Point},
+Is Graduated: {IsGraduated}
+");
+        }
+
+        public void CheckGraduation()
+        {
+            if (IsGraduated) Console.WriteLine("Graduated");
+            else Console.WriteLine("Not Graduated");
+        }
+
+        public void GetDiplomDeg()
+        {
+            if (Point <= 65)Console.WriteLine("No diploma");
+            
+            else if (Point <= 80) Console.WriteLine("General");
+            
+            else if (Point <= 90)Console.WriteLine("Honor");
+            
+            else Console.WriteLine("High honor");
+        }
+
+    }
+}
---
+++ ClassMethod/Program.cs
@@ -0,0 +1,167 @@
+﻿using ClassMethod.Models;
+
+namespace ClassMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TASK 1
+            //Student student = new Student("Adil", "Nasirli", "681s1", 94);
+            //student.GetInfo();
+            //student.CheckGraduation();
+            //student.GetDiplomDeg();
+
+            // TASK 2 Dersde yazilib
+
+            // TASK 3
+            //string text = "";
+            //var arr = FillArray(text);
+            //ShowArray(arr);
+
+            // TASK 4 
+            //string text = Console.ReadLine();
+
+            //Console.Write(Trim(text));
+
+            //Console.Write(TrimStart(text));
+
+            //Console.Write(TrimEnd(text));
+
+            // TASK 5
+            //Console.WriteLine("arrayin uzunlugunu daxil edin");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Fill array 1");
+            //int[,] nums1 = FillArray(n);
+            //Console.WriteLine("Fill array 1");
+            //int[,] nums2 = FillArray(n);
+
+            //Console.WriteLine("Array 1");
+            //ShowArray(nums1, n);
+            //Console.WriteLine("Array 2");
+            //ShowArray(nums2, n);
+
+            //var sum = Sum(nums1, nums2, n);
+            //Console.WriteLine("arraylarin cemi");
+            //ShowArray(sum, n);
+
+        }
+
+        // TASK 3
+        public static string[] FillArray(string text)
+        {
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                    count++;
+            }
+            string[] arr = new string[count + 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (text[j] != ' ')
+                        arr[i] += text[j];
+                    else i++;
+                }
+            }
+            return arr;
+        }
+        public static void ShowArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        // TASK 4
+        public static string Trim(string text)
+        {
+            string result = "";
+            TrimStart(text);
+            TrimEnd(text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                    result += text[i];
+            }
+            return result;
+        }
+        public static string TrimEnd(string text)
+        {
+            string result = "";
+            int endIndex = text.Length - 1;
+
+            while (endIndex >= 0 && text[endIndex] == ' ')
+            {
+                endIndex--;
+            }
+
+            for (int i = 0; i < endIndex; i++)
+            {
+                result += text[i];
+
+            }
+            return result;
+        }
+        public static string TrimStart(string text)
+        {
+            string result = "";
+            int startIndex = 0;
+
+            while (startIndex < text.Length && text[startIndex] == ' ')
+            {
+                startIndex++;
+            }
+
+            for (int i = startIndex; i < text.Length; i++)
+            {
+                result += text[i];
+
+            }
+            return result;
+        }
+
+        // TASK 5
+        public static int[,] FillArray(int n)
+        {
+            int[,] nums = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    nums[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return nums;
+        }
+        public static void ShowArray(int[,] arr, int n)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int[,] Sum(int[,] arr1, int[,] arr2, int n)
+        {
+            int[,] nums = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    nums[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+            return nums;
+        }
+    }
+}
